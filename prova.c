b13d0ce9cf2451a475033e2e3835782ed22c3398
jux89 <j_u_x@hotmail.com>
2016-09-30 19:54:06 -0300

    provacomcabecalho

--- prova.c
+++ prova.c
@@ -1,1 +1,24 @@
-ss
+// <---><---><---><---><---><---><---><---><---><---><---><---><---><---><---><---><---><---><--->
+// PROGRAMA COM MENU INTERATIVO PARA CRIACAO DE REGISTROS
+// 
+// Autor(a): Alana Azevedo, Ezequiel Drews e Juliana Xavier
+// Data: 30/09/2016
+// <---><---><---><---><---><---><---><---><---><---><---><---><---><---><---><---><---><---><--->
+
+#include <stdio.h>
+
+int main (){
+	
+	int opcao;
+	
+	switch (opcao){
+		
+		case 1: 
+		
+	}
+		case 2:	
+	}
+		case 3:
+	}
+		case 4:
+	}	
